782aa89902670c2773dbed8604535aca2a6ec691
mxjesse@163.com <mxjesse@163.com>
2020-05-08 02:03:15 +0800

    提交使用mapper接口的测试代码。

--- src/test/java/com/mx/dao/IUserDaoTest.java
+++ src/test/java/com/mx/dao/IUserDaoTest.java
@@ -7,6 +7,7 @@ import com.mx.pojo.User;
 import com.mx.sqlSession.SqlSession;
 import com.mx.sqlSession.SqlSessionFactory;
 import com.mx.sqlSession.SqlSessionFactoryBuilder;
+import org.dom4j.DocumentException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,7 +33,7 @@ public class IUserDaoTest {
         user.setId(1);
         user.setUsername("mx");
 
-        List<User> users = sqlSession.getAll("user.findAll", user);
+        List<User> users = sqlSession.selectList("user.findAll", user);
 
 //        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 //        List<User> users = mapper.findAll();
@@ -40,5 +41,35 @@ public class IUserDaoTest {
         for (int i = 0; i < users.size(); i++) {
             System.out.println(users.get(i));
         }
+    }
+
+    @Test
+    public void selectOne() throws Exception {
+
+        InputStream inputStream = Resources.getSourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        User user = new User();
+        user.setUsername("xiaoming");
+
+        User u = sqlSession.selectOne("user.findAll", user);
+        System.out.println(u);
+    }
+
+    @Test
+    public void getMapper() throws PropertyVetoException, DocumentException {
+
+        InputStream inputStream = Resources.getSourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
     }
 }
